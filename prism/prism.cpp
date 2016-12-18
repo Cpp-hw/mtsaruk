bc5d5d208e873c42a2df8943a822ed3afd57e927
Nicholas Tsaruk <fbt.ksnv@gmail.com>
2016-12-18 10:55:57 -0500

    Beginning: project is just created

---
+++ prism/prism.cpp
@@ -0,0 +1,13 @@
+// prism.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	cout<<"Hello, user. I am prism generator.\n";
+	return 0;
+}
+
