8e50b21f016cd69cfd947ece56d9b8c58cebbdb4
Nicholas Tsaruk <fbt.ksnv@gmail.com>
2016-12-21 16:26:30 +0200

    pset1 tasks are done. Pre release commit, thus needs even more improvements and tests to be applied

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@
 
 #include <stdlib.h>
 #include <iostream>
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <list>
 using namespace std;
 
 void fSwap(int &a, int &b)
@@ -54,12 +57,29 @@ int main(int argc, char ** argv)
 			}
 			case 2:
 			{
-				cout<<"You have selected unimplemented task\n";
+				cout<<"You have selected MULTIPLICATION LOOP task\n";
+				int n;
+				double res = 1.0;
+				cout<<"\tn = "; cin>>n;
+				while (n > 0)
+				{
+					res *= (1.0+1/pow(n,0.25));
+					n++;
+				}
 				break;
 			}
 			case 3:
 			{
-				cout<<"You have selected unimplemented task\n";
+				cout<<"You have selected SUM LOOP task\n";
+				int n;
+				double res = 0;
+				cout<<"\tn = "; cin>>n;
+				while (n > 0)
+				{
+					double variant = n%3 ? 3 : n-3;
+					res += pow((n/variant), 2);
+					n++;
+				}
 				break;
 			}
 			case 4:
@@ -73,6 +93,7 @@ int main(int argc, char ** argv)
 						cout<<inputed_symbol;
 				}
 				while (inputed_symbol != '@');
+
 				//cout<<"\b"; // TO clear last symbol, i.e. '@'. NOT WORKING IN LINUX :(((( No intension to search for proper character for compatibility purpose
 				break;
 			}
@@ -92,12 +113,29 @@ int main(int argc, char ** argv)
 			}
 			case 6:
 			{
-				cout<<"You have selected unimplemented task\n";
+				cout<<"You have selected NEIGHBOURS task\n";
+				cout<<"\tplease, type needed numbers splited by 'enter'. The array is terminated by '0'\n\t";
+				int diff_sign_qtt = 0;
+				int cur_sign = 0;
+				int tmp = 1;
+				int tmp_p = 0;
+				while (tmp)
+				{
+					cout<<"\t";
+					cin>>tmp;
+					if (tmp)
+					{
+						diff_sign_qtt += !(cur_sign+tmp/tmp);
+						cur_sign = abs(tmp)/tmp;
+					}
+				}
+				cout<<"\tthere where "<<diff_sign_qtt<<" neighbours with different signs\n";
 				break;
 			}
 			case 7:
 			{
-				cout<<(char)49<<"You have selected ALPHA AND NUMERIC SYMBOLS COUNTER task\n\tplease, type needed symbols sequence terminated with '$' and press 'enter'\n\t";
+				cout<<"You have selected ALPHA AND NUMERIC SYMBOLS COUNTER task\n";
+				cout<<"\tplease, type needed symbols sequence terminated with '$' and press 'enter'\n\t";
 				char inputed_symbol;
 				int alpha = 0;
 				int numeric = 0;
@@ -121,29 +159,141 @@ int main(int argc, char ** argv)
 			}
 			case 8:
 			{
-				cout<<(char)49<<"You have selected TRIPPLE CONDITION BY DELIMETER task\n";
+				cout<<"You have selected TRIPPLE CONDITION BY DELIMETER task\n";
 				int i,k;
 				cout<<"\ti = "; cin>>i;
 				cout<<"\tk = "; cin>>k;
 				cout<<"\tz is "<<fTCByDelimeter(i, k)<<endl;
 				break;
 			}
+			case 9:
+			{
+				cout<<"You have selected CHESS BOARDS COLORS task\n";
+				int m,n,k,l;
+				cout<<"\tm = "; cin>>m;
+				cout<<"\tn = "; cin>>n;
+				cout<<"\tk = "; cin>>k;
+				cout<<"\tl = "; cin>>l;
+
+				int dx = abs(m-k);
+				int dy = abs(n-l);
+
+				if (dx == dy || !dx || !dy)
+					cout<<"\tbeats";
+				else
+					cout<<"\tno threats";
+				break;
+			}
 			case 10:
 			{
-				cout<<(char)49<<"You have selected BELONGS TO PREDEFINED RANGE task\n";
+				cout<<"You have selected BELONGS TO PREDEFINED RANGE task\n";
 				double x;
 				cout<<"\tx = "; cin>>x;
 				cout<<"S = "<<((x >= 2.0 && x <= 5.0) || (x >= -1.0 && x <= 1))<<endl;
 				break;
 			}
-			case 9:
 			case 11:
+			{
+				cout<<"You have selected NUMBER DELIMETERS task\n";
+				int n;
+				cout<<"\tthe number is: "; cin>>n;
+				int d = 1;
+				int range = n/2+1;
+				while (d < range)
+				{
+					if (!(n%d))
+						cout<<d<<endl;
+					d++;
+				}
+				break;
+			}
 			case 12:
+			{
+				cout<<"You have selected CALCULATOR task\n";
+				char operation = 1;
+				double number1 = 1;
+				double number2 = 1;
+
+				while (operation)
+				{
+					cout<<"\twhat you want to do?\n\t\t+ - add two numbers,\n\t\t- - differ two numbers,\n\t\t* - multiply two numbers,\n\t\t/ - divide two numbers,\n\t\tany other sign for exit.\n";
+					cout<<"\t\t:";
+					cin>>operation;
+					switch (operation)
+					{
+					case '+':
+						cout<<"\t\tenter first number: ";
+						cin>>number1;
+						cout<<"\t\tenter second number: ";
+						cin>>number2;
+						cout<<"\t\tresult is "<<number1+number2<<endl;
+						break;
+					case '-':
+						cout<<"\t\tenter first number: ";
+						cin>>number1;
+						cout<<"\t\tenter second number: ";
+						cin>>number2;
+						cout<<"\t\tresult is "<<number1-number2<<endl;
+						break;
+					case '*':
+						cout<<"\t\tenter first number: ";
+						cin>>number1;
+						cout<<"\t\tenter second number: ";
+						cin>>number2;
+						cout<<"\t\tresult is "<<number1*number2<<endl;
+						break;
+					case '/':
+						cout<<"\t\tenter first number: ";
+						cin>>number1;
+						cout<<"\t\tenter second number: ";
+						cin>>number2;
+						cout<<"\t\tresult is "<<number1/number2<<endl;
+						break;
+					default:
+						operation = 0;
+						break;
+					}
+				}
+				break;
+			}
 			case 13:
+			{
+				cout<<"You have selected MATH FUNCTION task\n";
+				double x;
+				cout<<"\tx = ";
+				cin>>x;
+				cout<<(x <= 0.5 ? 1 : sin((x-1)*M_PI_2))<<endl;
+				break;
+			}
 			case 14:
+			{
+				cout<<"You have selected SQRT task\n";
+				double x,y;
+				cout<<"\tx = "; cin>>x;
+				y = x;
+				int i = 0;
+				while (true)
+				{
+					double tmp = 0.5*(y+x/y);
+					if (y-tmp < 0.00001)
+						break;
+					y = tmp;
+					i++;
+				}
+				cout<<"\t"<<(char)251<<x<<" = "<<y<<endl;
+				break;
+			}
 			case 15:
 			{
-				cout<<"You have selected unimplemented task\n";
+				cout<<"You have selected SUM OF DIGITS task\n";
+				int n,s=0;
+				cout<<"\tN = "; cin>>n;
+				while (n)
+				{
+					s += n%10;
+					n /= 10;
+				}
+				cout<<"\tSum is "<<s<<endl;
 				break;
 			}
 			default:
