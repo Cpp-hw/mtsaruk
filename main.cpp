55d9c614f8fa7afdb6d8ef2e89da4e75fb92ef8b
Nicholas Tsaruk <Nicholas Tsaruk>
2016-12-23 16:36:09 +0200

    pset2 halfway

--- main.cpp
+++ main.cpp
@@ -12,6 +12,30 @@
 #include <cmath>
 using namespace std;
 
+void fDrawTriangle(int ** triangle, int depth)
+{
+	int i = 0;
+	while (i < depth)
+	{
+		int j = 0;
+
+
+		while (j <= depth-i)
+		{
+			cout<<" ";
+			j++;
+		}
+
+		j = 0;
+		while (j <= i)
+		{
+			cout<<triangle[i][j]<<" ";
+			j++;
+		}
+		cout<<endl;
+		i++;
+	}
+}
 
 int main(int argc, char ** argv)
 {
@@ -36,29 +60,99 @@ int main(int argc, char ** argv)
 				cout<<"You have selected TRIANGLE ALGORYTHM task\n";
 
 				int ** triangle;
-
-				/////////////////////////
-				// user fills triangle //
-				cout<<"\tplease, specify triangle depth (number of rows) and press 'enter'\n\t";
+				int ** triangle_sums; // for performance purposes, this approach will secure n^2 execution time, otherwise execution is 2^n (much slower), where 'n' is number of triangle rows
 				int depth;
+
+				/////////////////////////////////////////////////
+				// specifying and filling triangle with numbers//
+				cout<<"\tplease, specify triangle depth (number of rows) and press 'enter'\n\t";
 				cin >> depth;
 				triangle = new int* [depth];
+				triangle_sums = new int* [depth];
+				cout<<"\twould you like to fill the triangle with random numbers? y - for yes, any other - for no\n\t";
+				char user_choice;
+				cout<<"\t"; cin >> user_choice;
+				if (user_choice == 'y')
+					srand(time(NULL));
 				int i = 0;
 				while (i < depth)
 				{
-					cout<<"\t\tplease, put row "<<i+1<<" numbers\n";
 					triangle[i] = new int [i+1];
+					triangle_sums[i] = new int [i+1];
+
+					if (user_choice != 'y')
+						cout<<"\t\tplease, put row "<<i+1<<" numbers\n";
 					int j = 0;
 					while (j <= i)
 					{
-						cout<<"\t\t\t"<<j+1<<": ";
-						cin>>triangle[i][j];
+						if (user_choice != 'y')
+						{
+							cout<<"\t\t\t"<<j+1<<": ";
+							cin>>triangle[i][j];
+						}
+						else
+							triangle[i][j] = rand()%10;
+						triangle_sums[i][j] = triangle[i][j];
 						j++;
 					}
 					i++;
 				}
-				// user fills triangle //
-				/////////////////////////
+				// specifying and filling triangle with numbers//
+				/////////////////////////////////////////////////
+
+				fDrawTriangle(triangle, depth);
+
+
+				/////////////////////////////////////
+				// searching for shortest way down //
+				i = depth-2;
+				while (i >= 0) // instead of 'while (i)' in case of negative 'i'
+				{
+					int j = 0;
+					while (j <= i)
+					{
+						triangle_sums[i][j] += triangle_sums[i+1][j] < triangle_sums[i+1][j+1] ? triangle_sums[i+1][j] : triangle_sums[i+1][j+1];
+						j++;
+					}
+					i--;
+				}
+				// searching for shortest way down //
+				/////////////////////////////////////
+
+				/////////////////////
+				// display results //
+				if (depth)
+					cout<<"\ttotal path length is "<<triangle_sums[0][0];
+
+				cout<<"\tthe path is: "<<triangle[0][0];
+				i = 1;
+				int j = 0;
+				while (i < depth)
+				{
+					if (triangle_sums[i][j] > triangle_sums[i][j+1])
+						j++;
+					cout<<", "<<triangle[i][j];
+					i++;
+				}
+				// display results //
+				/////////////////////
+
+
+
+				//////////////////
+				// clear memory //
+				int i = 0;
+				while (i < depth)
+				{
+					delete [] triangle[i];
+					delete [] triangle_sums[i];
+					i++;
+				}
+				delete [] triangle;
+				delete [] triangle_sums;
+				// clear memory //
+				//////////////////
+
 				break;
 			}
 			case 3:
@@ -94,17 +188,33 @@ int main(int argc, char ** argv)
 			case 5:
 			{
 				cout<<"You have selected WORD PATTERN task\n";
+
+				string str, pattern;
+				int s_l = str.length(), p_l = pattern.length();
+
+				cout<<"\toriginal string:\n\t";	cin>>str;
+				cout<<"\tpattern:\n\t";			cin>>pattern;
+
+				int * normalized_pattern =	new int[p_l];
+				int * tmp_pattern =			new int[p_l];
+
+				///////////////////////////
+				// pattern normalization //
+				int i = 0;
+				// pattern normalization //
+				///////////////////////////
+
+				delete [] normalized_pattern;
+				delete [] tmp_pattern;
 				break;
 			}
 			case 6:
 			{
 				string str;
-				unsigned int depth;
+				int depth;
 				cout<<"You have selected ZIGZAG CONVERSION task\n";
-				cout<<"\tplease, type needed symbols sequence and press 'enter'\n\t";
-				cin>>str;
-				cout<<"\tthank you. Please, enter needed zigzag depth (width) now\n\t";
-				cin>>depth;
+				cout<<"\tplease, type needed symbols sequence and press 'enter'\n\t";	cin>>str;
+				cout<<"\tthank you. Please, enter needed zigzag depth (width) now\n\t";	cin>>depth;
 				cout<<"\tconverted string is:\"";
 
 				int i = 0;
@@ -113,8 +223,8 @@ int main(int argc, char ** argv)
 				while ( i < depth )
 				{
 					int j = i;
-					int sb = s-2*i;
-					int sm = 2*i;
+					int sb = s-2*i; // additional variable in performance purpose, i.e., no need in repeated math operation in below loop
+					int sm = 2*i; // additional variable in performance purpose, i.e., no need in repeated math operation in below loop
 					while (j < l)
 					{
 						if (sb)
